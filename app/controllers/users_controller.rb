2156ceaca8bfddb0d8aae189f5469b9f217c6715
Erin Ponsonby <erinponsonby@gmail.com>
2019-12-12 13:08:52 -0500

    Update Users Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            puts @user
             render :json => @user
         else
             render :json => { :errors => @user.errors }, status: 400
