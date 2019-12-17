8d999df954bb6cce1fe48fec7516ac2755681879
Erin Ponsonby <erinponsonby@gmail.com>
2019-12-17 15:54:45 -0500

    Update game json

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,7 +13,7 @@ class GamesController < ApplicationController
     def create
         @game = Game.new(game_params)
         if @game.save
-            render :json => @game
+            render :json => @game, :include => {:user => {:only => :name}}
         else
             render :json => { :errors => @game.errors }, status: 400
         end
